fc919d134130ac3b41dad71e756259f2c26d7f04
tleq <thaile.isme@gmail.com>
2021-08-05 13:06:22 +0700

    update

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 from selenium import webdriver
 from time import sleep
-#----
-def get_text(count):
+
+
+def get_text(driver, count):
     word = driver.find_element_by_xpath('//*[@id="row1"]/span['+str(count)+']')
     return word.text+' '
+
+
 def send_texts(text, text_area):
     for c in text:
         text_area.send_keys(c)
+
+
 def main():
+    # Open website
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('https://10fastfingers.com/typing-test/english')
+
+    # Allow cookies time
     sleep(5)
+
+    # Main process
     count = 1
     text_area = driver.find_element_by_xpath('//*[@id="inputfield"]')
     while count < 382:
-        send_texts(get_text(count), text_area)
+        send_texts(get_text(driver, count), text_area)
         count += 1
+
+
 if __name__ == '__main__':
-    driver = webdriver.Chrome('./chromedriver')
-    driver.get('https://10fastfingers.com/typing-test/english')
     main()
-
-
-
-
-
-
-
-# prefs = {"profile.default_content_setting_values.notifications" : 2}
-# options = webdriver.ChromeOptions()
-# options.add_argument("--disable-infobars")
-# options.add_experimental_option("prefs", prefs)
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-# options.add_experimental_option('useAutomationExtension', False)
